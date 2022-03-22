7888c181f55496f93e89e29abc72e6b6f5c04a05
Khoa <leminkhoa@gmail.com>
2022-03-22 10:19:23 +0700

    First commit after finish ETL Pipeline

--- data/process_data.py
+++ data/process_data.py
@@ -1,31 +1,59 @@
 import sys
-
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
-
+    # Load raw data
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    # Combine dataframes
+    df = messages.merge(categories, on='id')
+    return df
 
 def clean_data(df):
-    pass
+    # Create a dataframe of individual category columns
+    categories = df['categories'].str.split(';', expand=True)
+    row = categories.iloc[0, :]
+    category_colnames = row.apply(lambda string: string[:-2]) # Exclude the last 2 characters, ex: related[-1], request[-0]
+    categories.columns = category_colnames
 
+    # Convert category values to just numbers 0 to 1
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str[-1:]
+    
+        # convert column from string to numeric
+        categories[column] = categories[column].astype('int64')
 
-def save_data(df, database_filename):
-    pass  
+    # Replace 'categories' column in df with new category columns
+    df = df.drop('categories', axis=1)
+    df = pd.concat([df, categories], axis=1)
 
+    # Drop duplicates
+    df = df.drop_duplicates()
+
+    return df
+
+def save_data(df, database_filepath):
+    engine = create_engine(f'sqlite:///{database_filepath}')
+    try:
+        df.to_sql('disaster_responses', engine, index=False)
+    except ValueError as err:
+        print("Table 'disaster_responses' already exists")
 
 def main():
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
 
-        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
-              .format(messages_filepath, categories_filepath))
+        print('Loading data...\n    MESSAGES: {messages}\n    CATEGORIES: {categories}'
+              .format(messages=messages_filepath, categories=categories_filepath))
         df = load_data(messages_filepath, categories_filepath)
 
         print('Cleaning data...')
         df = clean_data(df)
         
-        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        print('Saving data...\n    DATABASE: {database}'.format(database=database_filepath))
         save_data(df, database_filepath)
         
         print('Cleaned data saved to database!')
